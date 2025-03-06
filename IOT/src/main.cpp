7ea3fe6599573388243a78dd4f16231158a17610
Jonathan Kook <jkook95133@gmail.com>
2025-03-05 16:44:21 -0800

    edits to main.cpp

--- IOT/src/main.cpp
+++ IOT/src/main.cpp
@@ -7,52 +7,44 @@ const char* ucsdUsername = UCSD_USERNAME;
 const char* ucsdPassword = UCSD_PASSWORD;
 const char* wifiSsid = WIFI_SSID;
 const char* nonEnterpriseWifiPassword = NON_ENTERPRISE_WIFI_PASSWORD;
+// MQTT client - using descriptive client ID and topic
 const char* clientId = CLIENT_ID;
 const char* topicPrefix = TOPIC_PREFIX;
 
 Adafruit_BMP085 bmp;
-
 ECE140_MQTT mqtt(clientId, topicPrefix);
-ECE140_WIFI wifiManager;
+ECE140_WIFI wifi;
 
 void setup() {
     Serial.begin(115200);
     
+    // Connect to BMP085 Sensor
     if (!bmp.begin()) {
-        Serial.println("Could not find a valid BMP085 sensor, check wiring!");
-        while (1) {}
-      }
-
-    wifiManager.connectToWPAEnterprise(wifiSsid, ucsdUsername, ucsdPassword);
-    // wifiManager.connectToWiFi(wifiSsid, nonEnterpriseWifiPassword);
+        Serial.println("[Main] Could not find a valid BMP085 sensor");
+    }
 
     // Connect to MQTT Broker
-    if (mqtt.connectToBroker()) {
-        Serial.println("[ESP32] MQTT Connection Successful!");
-    } else {
-        Serial.println("[ESP32] MQTT Connection Failed!");
+    if (!mqtt.connectToBroker()) {
+        Serial.println("[Main] Failed to connect to MQTT broker");
     }
-    
+
+    // Connect to WiFi
+    // wifi.connectToWPAEnterprise(wifiSsid, ucsdUsername, ucsdPassword);
+    wifi.connectToWiFi(wifiSsid, nonEnterpriseWifiPassword);
 }
 
 void loop() {
+    mqtt.loop();
     float tempValue = bmp.readTemperature();
     float pressureValue = bmp.readPressure();
-    unsigned long timestamp = millis();
 
-    String message = "{";
-    message += "\"temperature\":" + String(tempValue) + ",";
-    message += "\"pressure\":" + String(pressureValue);
-    message += "}";
+    String payload = "{";
+    payload += "\"temperature\":" + String(tempValue) + ",";
+    payload += "\"pressure\":" + String(pressureValue);
+    payload += "}";
 
-    if (mqtt.publishMessage("readings", message)) {
-        Serial.println("[ESP32] Sensor data published successfully: " + message);
-    } 
-    else {
-        Serial.println("[ESP32] Failed to publish sensor data");
-    }
-    mqtt.loop();
-    delay(5000);
+    mqtt.publishMessage("readings", payload);
 
-    
+    // Delay for 2 seconds
+    delay(2000); 
 }
