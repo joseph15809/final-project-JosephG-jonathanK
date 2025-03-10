5850f132890f8c42c39547b75221ff0f346e10d1
Jonathan Kook <jkook95133@gmail.com>
2025-03-10 16:53:36 -0700

    fast api query

--- app/main.py
+++ app/main.py
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
 from fastapi import FastAPI, Request, Response, HTTPException, Query
-from fastapi.responses import Response, HTMLResponse, RedirectResponse
-=======
-from fastapi import FastAPI, Request, Response, HTTPException
 from fastapi.responses import Response, HTMLResponse, RedirectResponse, JSONResponse
->>>>>>> wardrobe
 from fastapi.staticfiles import StaticFiles
 import uvicorn
 import os
@@ -24,14 +19,11 @@ from .database import (
     get_session,
     delete_session,
     add_user,
-<<<<<<< HEAD
     get_db_connection,
     add_temperature,
-    clear_database
-=======
+    clear_database,
     add_clothes,
     remove_clothes
->>>>>>> wardrobe
 )
 
 @asynccontextmanager
@@ -105,11 +97,7 @@ async def signup(request: Request):
         return {"error": f"Signup failed: {e}"}
 
     # Set cookie with session ID
-<<<<<<< HEAD
     response = RedirectResponse(url="/dashboard", status_code=302)
-=======
-    response = RedirectResponse(url=f"/dashboard", status_code=302)
->>>>>>> wardrobe
     response.set_cookie(
         key="session_id",
         value=session_id,
@@ -131,11 +119,7 @@ async def login_html(request: Request):
         if session:
             user = await get_user_by_id(session["user_id"])
             if user:
-<<<<<<< HEAD
                 return RedirectResponse(url="/dashboard", status_code=302)
-=======
-                return RedirectResponse(url=f"/dashboard", status_code=302)
->>>>>>> wardrobe
     return HTMLResponse(content=read_html("app/static/login.html"))
 
 
@@ -156,11 +140,7 @@ async def login(request: Request):
     await create_session(user["user_id"], session_id)
 
     # Set cookie with session ID
-<<<<<<< HEAD
     response = RedirectResponse(url="/dashboard", status_code=302)
-=======
-    response = RedirectResponse(url=f"/dashboard", status_code=302)
->>>>>>> wardrobe
     response.set_cookie(
         key="session_id",
         value=session_id,
@@ -185,14 +165,8 @@ async def logout(request: Request):
     response.delete_cookie("session_id")
     return response
 
-<<<<<<< HEAD
-
-@app.get("/dashboard", response_class=HTMLResponse)
-async def user_dashboard(request: Request):
-=======
 @app.get("/wardrobe", response_class=HTMLResponse)
 async def user_wardrobe(request: Request):
->>>>>>> wardrobe
     """Show user profile if authenticated, error if not"""
     session_id = request.cookies.get("session_id")
 
@@ -316,18 +290,12 @@ async def user_page(request: Request):
         return RedirectResponse(url="/login", status_code=302)
 
     user_id = session["user_id"]
-<<<<<<< HEAD
-    user = await get_user_by_id(user_id)
-    if not user or user["user_id"] != user_id:
-        return {"error": f"Not authenticated as {user['name']}"}
-=======
     if not user_id:
         return RedirectResponse(url="/login", status_code=302)
     
     user = await get_user_by_id(user_id)
     if not user:
         return {"error": "User not found"}
->>>>>>> wardrobe
 
     return HTMLResponse(content=read_html("app/static/dashboard.html"))
     
@@ -514,10 +482,4 @@ async def signup_html(request: Request):
     return RedirectResponse(url="/login", status_code=302)
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     uvicorn.run(app="app.main:app", host="0.0.0.0", port=8000, reload=True)
-=======
-   uvicorn.run(app="app.main:app", host="0.0.0.0", port=6543, reload=True)
-
-   
->>>>>>> wardrobe
