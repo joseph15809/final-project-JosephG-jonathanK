825d0cf1cfcbdaf8d590a1a7b024ce70970d8757
Joseph Gonzalez <joseph15809@gmail.com>
2025-03-13 17:37:17 -0700

    database

--- app/database.py
+++ app/database.py
@@ -30,9 +30,12 @@ def get_db_connection(
         try:
             connection = mysql.connector.connect(
                 host=os.getenv("MYSQL_HOST"),
+                port=int(os.getenv('MYSQL_PORT')),
                 user=os.getenv("MYSQL_USER"),
                 password=os.getenv("MYSQL_PASSWORD"),
-                database=os.getenv("MYSQL_DATABASE")
+                database=os.getenv("MYSQL_DATABASE"),
+                ssl_ca=os.getenv('MYSQL_SSL_CA'),  # Path to CA certificate file
+                ssl_verify_identity=True
             )
 
             # Test the connection
